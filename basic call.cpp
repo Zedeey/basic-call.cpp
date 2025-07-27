afdb29bebc95ce18302f087e5a088ab41cf755e3
Zedeey <cedkent2000@gmail.com>
2025-07-27 20:52:05 +0800

    Add files via upload

---
+++ basic call.cpp
@@ -0,0 +1,36 @@
+#inlcude <iostream>
+using namespace std;
+
+int main ()
+{
+
+    int num1, num2
+    char operation;
+    
+    cin >> num1
+    cin >> num2
+
+    switch (operation){
+        case '+':
+        cout << "Result: " << num1 + num 2 << endl;
+        break:
+
+        case '-':
+        cout << "Result: " << num1 - num2 << endl;
+        break;
+
+        case '/':
+        cout << "Result: " << num1 / num2 << endl;
+        break;
+        
+        case '*':
+        cout << "Result: " << num1 *num2 << endl;
+        break:
+        
+        default:
+        cout << "invalid operation:" << operatioon << endl;
+        
+     } 
+
+    return 0;
+}
